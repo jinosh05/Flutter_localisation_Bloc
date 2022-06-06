2b99a46e8f1dcb7491ddf95851b7e04b3f6b59cc
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-06-06 15:27:49 +0530

    LanguageState updated

--- lib/blocs/bloc/language_state.dart
+++ lib/blocs/bloc/language_state.dart
@@ -1,6 +1,9 @@
 part of 'language_bloc.dart';
 
-@immutable
 abstract class LanguageState {}
 
-class LanguageInitial extends LanguageState {}
+class LanguageLoaded extends LanguageState {
+  final Locale locale;
+
+  LanguageLoaded(this.locale);
+}
