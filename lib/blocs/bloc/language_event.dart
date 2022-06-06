34bc5e8d83cafa2e298a4c311df37e508caca2ca
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-06-06 15:31:33 +0530

    ToggleLanguageEvent created

--- lib/blocs/bloc/language_event.dart
+++ lib/blocs/bloc/language_event.dart
@@ -2,3 +2,9 @@ part of 'language_bloc.dart';
 
 @immutable
 abstract class LanguageEvent {}
+
+class ToggleLanguageEvent extends LanguageEvent {
+  final LanguageEvent language;
+
+  ToggleLanguageEvent(this.language);
+}
--- lib/blocs/bloc/language_state.dart
+++ lib/blocs/bloc/language_state.dart
@@ -1,9 +1,12 @@
 part of 'language_bloc.dart';
 
-abstract class LanguageState {}
+@immutable
+abstract class LanguageState {
+  const LanguageState();
+}
 
 class LanguageLoaded extends LanguageState {
   final Locale locale;
 
-  LanguageLoaded(this.locale);
+  const LanguageLoaded(this.locale);
 }
